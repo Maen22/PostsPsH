706ce49b80df7475e1218e045b7b6b96717a528b
Maen22 <m3n.ibregeth@gmail.com>
2021-05-29 05:00:56 +0300

    added angular material.

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -3,13 +3,17 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-post-create',
   templateUrl: './post-create.component.html',
-  styleUrls: ['./post-create.component.css']
+  styleUrls: ['./post-create.component.css'],
 })
 export class PostCreateComponent implements OnInit {
+  newPost = 'No Content';
+  enteredValue = '';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  onAddPost() {
+    this.newPost = this.enteredValue;
   }
 
+  ngOnInit(): void {}
 }
