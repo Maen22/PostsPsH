e2def699f6a1808dd6f6bc6740c7a4fff93ef58a
Maen22 <m3n.ibregeth@gmail.com>
2021-06-02 19:53:35 +0300

    added image uplaod angular logic.

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -3,6 +3,7 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { Post } from '../post.model';
 import { PostsService } from '../posts.service';
+import { asyncMimeTypeValidator } from './mime-type.validator';
 
 @Component({
   selector: 'app-post-create',
@@ -15,6 +16,7 @@ export class PostCreateComponent implements OnInit {
   post: Post | undefined;
   isLoading = false;
   form: FormGroup | undefined;
+  imagePreview: string | undefined;
 
   constructor(
     private postsService: PostsService,
@@ -28,6 +30,11 @@ export class PostCreateComponent implements OnInit {
         validators: [Validators.required, Validators.minLength(3)],
       }),
       content: new FormControl(null, { validators: Validators.required }),
+      // this field is not binded to HTML because no need to show it
+      image: new FormControl(null, {
+        validators: [Validators.required],
+        asyncValidators: [asyncMimeTypeValidator],
+      }),
     });
 
     this.route.paramMap.subscribe((paramMap) => {
@@ -53,6 +60,17 @@ export class PostCreateComponent implements OnInit {
         this.postId = null;
       }
     });
+  }
+
+  onImagePick(event: Event) {
+    const file = (event.target as HTMLInputElement).files![0];
+    this.form?.patchValue({ image: file });
+    this.form?.get('image')?.updateValueAndValidity();
+    const reader = new FileReader();
+    reader.onload = () => {
+      this.imagePreview = reader.result as string;
+    };
+    reader.readAsDataURL(file);
   }
 
   onSavePost() {
