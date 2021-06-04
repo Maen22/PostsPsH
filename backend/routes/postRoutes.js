ec18a39abc827d57f27fa8f5b98f3b221ca748ef
Maen22 <m3n.ibregeth@gmail.com>
2021-06-04 22:53:55 +0300

    added multer config for storing images on server.

--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -1,7 +1,31 @@
 import express from "express";
 import Post from "../models/post.js";
+import multer from "multer";
 
 const router = express.Router();
+
+const MIME_TYPE_MAP = {
+  "image/png": "png",
+  "image/jpeg": "jpg",
+  "image/jpg": "jpg",
+};
+
+// Multer configrations for saving images on the server
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    const isValid = MIME_TYPE_MAP[file.mimetype];
+    let error = new Error("Invalid mime type");
+    if (isValid) {
+      error = null;
+    }
+    cb(error, "backend/images");
+  },
+  filename: (req, file, cb) => {
+    const name = file.originalname.toLowerCase().split(" ").join("-");
+    const extension = MIME_TYPE_MAP[file.mimetype];
+    cb(null, name + "-" + Date.now() + "." + extension);
+  },
+});
 
 // Get Posts
 router.get("", (_, res) => {
