e827c659f6423216822ad7d2828f538d2e304963
mibregeth <43907401+Maen22@users.noreply.github.com>
2021-06-09 16:04:08 +0300

    added backend signup functionality.

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,5 +1,6 @@
 import express from "express";
-import Post from "../models/post.js";
+import bcrypt from "bcrypt";
+import User from "../models/user.js";
 
 const router = express.Router();
 
@@ -7,7 +8,29 @@ const router = express.Router();
 router.get("/login", (req, res) => {});
 
 // Signup Endpoint
-router.post("/signup", (req, res) => {});
+router.post("/signup", (req, res) => {
+  const { email, password } = req.body;
+
+  bcrypt.hash(password, 10).then((hash) => {
+    const user = new User({
+      email,
+      hash,
+    });
+    user
+      .save()
+      .then((result) => {
+        res.status(201).json({
+          message: "User registered succesfully.",
+          result,
+        });
+      })
+      .catch((err) => {
+        res.status(500).json({
+          error: err,
+        });
+      });
+  });
+});
 
 const userRoutes = router;
 
